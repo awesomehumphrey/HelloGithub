b53218c4ef3226e7fea9c2357e515ce3ddb122bb
awesomehumphrey <humphreyluv@gmail.com>
2015-06-03 20:16:35 +0100

    First draft of the simulation. Feeding test parameters into model.

---
+++ HopandAbs.R
@@ -0,0 +1,8 @@
+channels = c(ABC, ZTV, MTV, HDTV)
+Films = (horror, romance, drama, comedy, biopics, action)
+Caradverts = (ford = 0.7, nissan = 0.4)
+
+#Teenager: Probability of hopping based on no of breaks and no of adverts
+no = c(0,1,2,3,4,5,6,7,8,9)
+brkprob = c(0.05,0.08,0.12,0.15,0.21,0.34,0.55,0.7,0.9,0.95)
+advprob = c(0.03,0.04,0.11,0.13,0.15,0.23,0.3,0.34,0.39,0.55)
