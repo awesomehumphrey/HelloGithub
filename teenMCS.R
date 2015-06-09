da4bb067eb2f97cd396c220e98469b3c0cdee38b
awesomehumphrey <humphreyluv@gmail.com>
2015-06-09 19:43:57 +0100

    Stable basic version.

--- teenMCS.R
+++ teenMCS.R
@@ -1,12 +1,12 @@
-channels = c("ABC", "ZTV", "MTV", "HDTV")   #remove quote if given glue factor
+channels = c("ABC", "ZTV", "JTV", "HDTV")   #remove quote if given glue factor
 movie=c(horror=0.22, romance=0.25, drama=0.31, comedy=0.35, biopics=0.23, action=0.26)
 carAdverts = c(0.03,0.06,0.09,0.12,0.15,0.19,0.20,0.22)#adverts with glue factors
-toyAdverts = c(0.02,0.04,0.05,0.07,0.10,0.11,0.13,0.14)
+furnitureAdverts = c(0.02,0.04,0.05,0.07,0.10,0.11,0.13,0.14)
 cosmeticsAdverts = c(0.025,0.065,0.077,0.163,0.181,0.193,0.222,0.245)
 foodAdverts = c(0.01,0.075,0.08, 0.099,0.16,0.18,0.21,0.23,0.24,0.25)
 lenfilm = c(0.31,0.31,0.27,0.27,0.27,0.22,0.22,0.22,0.18,0.18)
 
-tolerance = 0.44 #Probability of teen hopping independent of other factors
+tolerance = 0.25 #Probability of teen hopping independent of other factors
 brkprob = c(0.39,0.39,0.49,0.49,0.49,0.49,0.49,0.49,0.55,0.55)
 advprob = c(0.35,0.39,0.39,0.39,0.44,0.44,0.44,0.44,0.44,0.48)
 
@@ -27,7 +27,7 @@ for (y in 1:100){
   nbrk = sample(brkprob,1000,replace = TRUE)#choosing break no at random
   nadvert = sample(advprob,1000,replace = TRUE)#choosing advert no at random
   nmovie = sample(movie,1000,replace = TRUE)#choosing movies no at random
-  gluef = sample(cosmeticsAdverts,1000,replace = TRUE)#choosing gluefactor  at random
+  gluef = sample(carAdverts,1000,replace = TRUE)#choosing gluefactor  at random
   len = sample(lenfilm,1000,replace = TRUE)#choosing advert no at random
 
   #hopa = ((len*nbrk*nadvert)/(0.38*0.28))-0
